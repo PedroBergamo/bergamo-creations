384b3af322ce566e007ebd3a29f754fba7ef6209
Pedro Bergamo <pedrobergamo89@gmail.com>
2022-11-24 12:22:12 +0200

    new deployment

--- node_modules/swiper/modules/a11y/a11y.js
+++ node_modules/swiper/modules/a11y/a11y.js
@@ -190,7 +190,13 @@ export default function A11y({
   };
 
   const handlePointerUp = () => {
-    swiper.a11y.clicked = false;
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => {
+        if (!swiper.destroyed) {
+          swiper.a11y.clicked = false;
+        }
+      });
+    });
   };
 
   const handleFocus = e => {
@@ -200,6 +206,7 @@ export default function A11y({
     const isActive = swiper.slides.indexOf(slideEl) === swiper.activeIndex;
     const isVisible = swiper.params.watchSlidesProgress && swiper.visibleSlides && swiper.visibleSlides.includes(slideEl);
     if (isActive || isVisible) return;
+    if (e.sourceCapabilities && e.sourceCapabilities.firesTouchEvents) return;
 
     if (swiper.isHorizontal()) {
       swiper.el.scrollLeft = 0;
