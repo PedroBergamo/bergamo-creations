11b0c65aa2b519be69a5623ce42b4b594c4f5fea
Pedro Bergamo <pedrobergamo89@gmail.com>
2023-04-12 13:51:17 +0300

    Adding splash screen

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,18 +2,22 @@ import React from 'react';
 import "../css/Header.css";
 import Menu from "./Menu.js";
 
+import {Row, Col} from "react-bootstrap"
 import Mark from "../brand/mark/bergamoth.png";
+import Frame from "../brand/frame-white-web.png";
 
 
 export default class Header extends React.Component {
     render() {
         return (
           <div className="header" id="myHeader">
+          <Row>
             <Menu/>
-            <div className="logo">
-              <img src={Mark} width="200px" alt="mark"/>
-                <h1>Bergamo Creations</h1>
-                <p>Eccentrify your identity</p>
+          </Row>
+            <div className="splash-elements">
+            <img src={Mark} width="40%" alt="mark"/>
+              <h1>Bergamo Creations</h1>
+              <p>Eccentrify your identity</p>
             </div>
           </div>
         );
